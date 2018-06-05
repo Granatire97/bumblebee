4068a892fe11c4aaa5517c1191c6febd00d8a8f5
Timothy Hartman <Timothy.Hartman@dcsg.com>
2018-06-05 14:49:20 -0400

    Create initial RESTful Service for Allocation Failures
    Created Application, Controller, and Service Classes that complete basic
    database functionality (connecting to and querying DB). Added
    dependencies in build.gradle and DB config in application.yaml.

---
+++ src/main/java/com/example/demo/ThresholdController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ThresholdController {
+
+	private @Autowired ThresholdService thresholdService;
+	
+	@RequestMapping(path = "/getAllocationFailures")
+	public HashMap<String,String> getAllocationFailures() throws IOException{
+		
+		Double allocationFailures = thresholdService.getAllocationFailures();
+		String priority = thresholdService.surpassesThreshold(allocationFailures) ? "BAD":"GOOD";
+		HashMap<String, String> response = new HashMap<String, String>();
+		response.put("priority", priority);
+		response.put("Failure Percentage", allocationFailures.toString());
+		return response;
+		
+	}
+	
+	
+	
+}
